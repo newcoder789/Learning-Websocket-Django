a5844cf2d3e7b94868c8b805fc09c2dfa727c8c5
newcoder789 <mymail@gmail.com>
2024-09-15 10:43:53 +0530

    websocket server done using node.js

---
+++ using_node/websocket-project/server.js
@@ -0,0 +1,24 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+  console.log('A new client connected');
+  
+
+  ws.send(JSON.stringify({ message: 'Welcome to the WebSocket server!' }));
+  // reply 
+  ws.on('message', (message) => {
+    if (message== 'hi'){
+      ws.send("hello");
+
+    }
+    else if(message=='bye'){
+      ws.send('goodbye!');
+    }
+  });
+
+
+});
+
+console.log('WebSocket server is running on ws://localhost:3000');
